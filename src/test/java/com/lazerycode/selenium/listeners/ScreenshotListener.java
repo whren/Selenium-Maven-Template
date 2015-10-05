4aa6b91ab2cd3364925dd1dcc5c5d2e13717c5ab
unknown <tanguy.de.lignieres@MW7P9MALQWIC2J.dir.svc.accenture.com>
2015-10-05 19:18:19 +0200

    Test webhook

--- src/test/java/com/lazerycode/selenium/listeners/ScreenshotListener.java
+++ src/test/java/com/lazerycode/selenium/listeners/ScreenshotListener.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import static com.lazerycode.selenium.DriverFactory.getDriver;
 
 public class ScreenshotListener extends TestListenerAdapter {
+
     private boolean createFile(File screenshot) {
         boolean fileCreated = false;
 
