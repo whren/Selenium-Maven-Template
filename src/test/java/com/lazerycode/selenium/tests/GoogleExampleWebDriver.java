e799ff2d60f64a17419cce6c621b3c6189496fc2
unknown <tanguy.de.lignieres@MW7P9MALQWIC2J.dir.svc.accenture.com>
2015-10-05 12:45:28 +0200

    Ajout d'un test "failed"

--- src/test/java/com/lazerycode/selenium/tests/GoogleExampleWebDriver.java
+++ src/test/java/com/lazerycode/selenium/tests/GoogleExampleWebDriver.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class GoogleExampleWebDriver extends DriverFactory {
 
@@ -82,5 +83,43 @@ public class GoogleExampleWebDriver extends DriverFactory {
 
         // Should see: "cheese! - Google Search"
         System.out.println("Page title is: " + driver.getTitle());
+    }
+
+    @Test
+    public void googleFailedExample() throws Exception {
+        // Create a new WebDriver instance
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        WebDriver driver = getDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.clear();
+        element.sendKeys("Failed!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+
+        // Google's search is rendered dynamically with JavaScript.
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("Failed!");
+            }
+        });
+
+        // Should see: "cheese! - Google Search"
+        System.out.println("Page title is: " + driver.getTitle());
+        Assert.assertEquals("Succeed! title was expected", Succeed!", driver.getTitle());
     }
 }
