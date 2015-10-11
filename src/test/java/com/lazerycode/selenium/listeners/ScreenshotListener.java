7066afbffb2393e0d2dba31d5e636f05248cca83
unknown <tanguy.de.lignieres@MW7P9MALQWIC2J.dir.svc.accenture.com>
2015-10-12 00:31:49 +0200

    correct import

--- src/test/java/com/lazerycode/selenium/listeners/ScreenshotListener.java
+++ src/test/java/com/lazerycode/selenium/listeners/ScreenshotListener.java
@@ -12,7 +12,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
-import java.util.Boolean;
 
 import static com.lazerycode.selenium.DriverFactory.getDriver;
 
